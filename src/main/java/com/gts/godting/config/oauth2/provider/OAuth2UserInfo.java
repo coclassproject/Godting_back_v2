de315a9f63f71e9468f852376e2c9e962c4d28bd
pooooobi <cyh4662@icloud.com>
2022-05-13 18:50:52 +0900

    feat: 코드 리빌딩을 위한 삭제

--- src/main/java/com/gts/godting/config/oauth2/provider/OAuth2UserInfo.java
+++
@@ -1,9 +0,0 @@
-package com.gts.godting.config.oauth2.provider;
-
-import java.util.Map;
-
-public interface OAuth2UserInfo {
-    Map<String, Object> getAttributes();
-    String getProviderId();
-    String getProvider();
-}
