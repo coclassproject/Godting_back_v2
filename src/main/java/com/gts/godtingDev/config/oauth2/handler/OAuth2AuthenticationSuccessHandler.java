e00d9adb33e42a04eb9aaebb282c01a56a99e432
pooooobi <cyh4662@icloud.com>
2022-05-15 23:04:56 +0900

    feat: OAuth2AuthenticationSuccessHandler 초기 구현

--- src/main/java/com/gts/godtingDev/config/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/gts/godtingDev/config/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,5 +1,12 @@
 package com.gts.godtingDev.config.oauth2.handler;
 
+import com.gts.godtingDev.config.oauth2.authentication.OAuth2UserDetails;
+import com.gts.godtingDev.user.User;
+import com.gts.godtingDev.user.UserRepository;
+import com.gts.godtingDev.user.oauth2.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -8,12 +15,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Value("${front.redirect.url}")
+    private String redirectUrl;
+    private final UserRepository userRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        super.onAuthenticationSuccess(request, response, authentication);
+        OAuth2UserDetails principal = (OAuth2UserDetails) authentication.getPrincipal();
+
+        if (authentication.getAuthorities().stream().anyMatch(s -> s.getAuthority().equals(Role.GUEST.getGratedAuthority()))){
+            getRedirectStrategy().sendRedirect(request, response, redirectUrl + "sign_in?social_type=" +
+                    principal.getSocialType() + "&social_id=" +
+                    principal.getSocialId());
+        } else {
+            Optional<User> user = userRepository.findBySocialTypeAndSocialId(principal.getSocialType(), principal.getSocialId());
+            // 토큰 생성 후 둘다 response에 담아서 보내기
+            // authentication에 토큰 생성 담기.
+        }
     }
 }
